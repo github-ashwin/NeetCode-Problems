6a81e7ee1d71dc12c7955e5dc70bb22787190a7a
github-ashwin <ashi.github@gmail.com>
2025-01-21 21:39:31 +0530

    Reverse Polish Notation

---
+++ NeetCode 150/Stack/ReversePolishNotation.py
@@ -0,0 +1,24 @@
+from typing import List
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+
+        for c in tokens:
+            if c == '+':
+                stack.append(stack.pop() + stack.pop())
+            elif c == '-':
+                n1 = stack.pop()
+                n2 = stack.pop()
+                stack.append(n2 - n1)
+            elif c == '*':
+                stack.append(stack.pop() * stack.pop())
+            elif c == '/':
+                n1 = stack.pop()
+                n2 = stack.pop()
+                stack.append(int(n2/n1))
+            else:
+                stack.append(int(c))
+        
+        return stack[0]
+            
+        
