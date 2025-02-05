22d0c6f1a26709bd06202b5f3e1e89556f45d2e3
github-ashwin <ashi.github@gmail.com>
2025-02-05 21:48:23 +0530

    P242 - Leetcode(CIR TASK)

---
+++ NeetCode 150/p242-leetcode.py
@@ -0,0 +1,13 @@
+from collections import Counter
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if Counter(s)==Counter(t):
+            return True
+        else:
+            return False
+        
