e3d74a2823fd7a2affdb5e3725cb9e884320685b
github-ashwin <ashi.github@gmail.com>
2025-07-19 19:59:44 +0530

    Valid Anagram

--- NeetCode 150/Arrays & Hashing/ValidAnagram.py
+++ NeetCode 150/Arrays & Hashing/ValidAnagram.py
@@ -1,35 +1,35 @@
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        slist = list(s)
-        tlist = list(t)
-        ds = {}
-        dt = {}
-        for i in slist:
-            if i in ds:
-                ds[i] = ds[i]+1
-            else:
-                ds[i] = 1
-        
-        for j in tlist:
-            if j in dt:
-                dt[j] = dt[j] + 1
-            else:
-                dt[j] = 1
-        
-        if ds==dt:
-            return True
-        else:
-            return False
-
-# Alternate Approach
-
-# from collections import Counter
-
 # class Solution:
 #     def isAnagram(self, s: str, t: str) -> bool:
-#         # Quick length check
-#         if len(s) != len(t):
-#             return False
+#         slist = list(s)
+#         tlist = list(t)
+#         ds = {}
+#         dt = {}
+#         for i in slist:
+#             if i in ds:
+#                 ds[i] = ds[i]+1
+#             else:
+#                 ds[i] = 1
         
-#         # Use Counter to compare character frequencies
-#         return Counter(s) == Counter(t)
+#         for j in tlist:
+#             if j in dt:
+#                 dt[j] = dt[j] + 1
+#             else:
+#                 dt[j] = 1
+        
+#         if ds==dt:
+#             return True
+#         else:
+#             return False
+
+from collections import Counter
+from typing import List
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # If length mismatch, not an anagram
+        if len(s) != len(t):
+            return False
+        
+        # Counter is used to find hashtable objects
+        return Counter(s) == Counter(t)
+
