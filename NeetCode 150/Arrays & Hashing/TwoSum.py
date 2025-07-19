7e8074752423e716c851e7f914a5f55b9231b647
github-ashwin <ashi.github@gmail.com>
2025-07-19 20:20:22 +0530

    Two Sum

--- NeetCode 150/Arrays & Hashing/TwoSum.py
+++ NeetCode 150/Arrays & Hashing/TwoSum.py
@@ -1,16 +1,25 @@
 from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # for i in range(len(nums)):
-        #     j = i+1
-        #     while j < len(nums):
-        #         if nums[i] + nums[j] == target:
-        #             return [i,j]
-        #         j = j+1
+        """
+        Works only if the list is sorted
+
+        i,j = 0,len(nums)-1
+        while i<j:
+            if nums[i] + nums[j] == target:
+                return [i,j]
+            elif nums[i] + nums[j] > target:
+                j-=1
+            else:
+                i+=1
+        """
+        hm = {} # Hashmap for storing key-value pair
+
+        for idx,val in enumerate(nums): # Enumerate through the list
+            req = target - val # Checks if the required second number is there in the hashmap already
+            if req in hm:
+                return[hm[req],idx] # If there, return the current index and the index of second number
+            else:
+                hm[val]=idx # Else, add the number into the hashmap
         
-        vals = {}
-        for i, n in enumerate(nums):
-            dif = target - n
-            if dif in vals:
-                return [vals[dif], i] 
-            vals[n] = i 
+
