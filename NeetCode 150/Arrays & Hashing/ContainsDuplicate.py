38921aad6914e60ed0d395546107dfc3df8c51e0
github-ashwin <ashi.github@gmail.com>
2025-07-19 19:54:40 +0530

    Contains Duplicate

--- NeetCode 150/Arrays & Hashing/ContainsDuplicate.py
+++ NeetCode 150/Arrays & Hashing/ContainsDuplicate.py
@@ -1,7 +1,7 @@
 from typing import List
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
-        if len(set(nums))==len(nums):
+        if len(nums) == len(set(nums)): # Set contains only distinct elements
             return False
         else:
             return True
