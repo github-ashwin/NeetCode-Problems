6193c4ccc2bb940a4ac349f21144119a0dfeaf24
github-ashwin <ashi.github@gmail.com>
2025-07-22 18:45:25 +0530

    Encode and Decode Strings

--- NeetCode 150/Arrays & Hashing/EncodeDecodeStrings.py
+++ NeetCode 150/Arrays & Hashing/EncodeDecodeStrings.py
@@ -1,24 +1,25 @@
 from typing import List
+
 class Solution:
 
     def encode(self, strs: List[str]) -> str:
+        # Length of the string + delimiter + string
         enc = ""
-        for i in strs:
-            enc+= str(len(i))+'/'+i #length of the string,delimiter,string
+        for string in strs:
+            length = len(string)
+            enc+=str(length)+"^"+string
         return enc
-
     def decode(self, s: str) -> List[str]:
-        dec = []
+        result = []
         i = 0
-
-        while i<len(s):
+        while i < len(s):
             j = i
-            while s[j] != '/':
-                j+=1
-            l = int(s[i:j])
-            i = j+1
-            j = i+l
-            dec.append(s[i:j])
-            i = j
+            while s[j] != '^':
+                j +=1
+            str_len = int(s[i:j])
+            start = j+1
+            end = start + str_len
+            result.append(s[start:end])
+            i = end
+        return result
         
-        return dec
