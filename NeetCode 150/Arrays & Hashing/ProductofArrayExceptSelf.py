a3780c0d545107753ec7bf23a6fbb2c640326a39
github-ashwin <ashi.github@gmail.com>
2025-07-23 18:47:49 +0530

    Product of Array Except Self

--- NeetCode 150/Arrays & Hashing/ProductofArrayExceptSelf.py
+++ NeetCode 150/Arrays & Hashing/ProductofArrayExceptSelf.py
@@ -1,34 +1,17 @@
 from typing import List
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # result = [0]*len(nums)
-        # for i in range(len(nums)):
-        #     prd = 1
-        #     for j in range(len(nums)):
-        #         if i==j:
-        #             continue
-        #         prd = prd*nums[j]
-        #     result[i] = prd
-        
-        # return result
-        
-        l = len(nums)
+        result = [0] * len(nums)
 
-        result = [1] * l
-
-        # first find prefixes
-
+        # Product except self is prefix product(self) * postfix product(self)
         prefix = 1
-
-        for i in range(l):
+        for i in range(len(nums)):
             result[i] = prefix
             prefix = prefix * nums[i]
 
-        # finding postifixes
-
         postfix = 1
-
-        for i in range(l -1,-1,-1):
-                result[i] = result[i] * postfix
-                postfix = postfix * nums[i]
-        return result 
+        for i in range(len(nums)-1,-1,-1):
+            result[i] = result[i] * postfix
+            postfix = postfix * nums[i]
+        
+        return result
