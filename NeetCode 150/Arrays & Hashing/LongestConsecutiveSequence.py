1d92b1998a651886123b91cd72645dc889b99ab3
github-ashwin <ashi.github@gmail.com>
2025-07-25 14:47:43 +0530

    Longest Consecutive Sequence

--- NeetCode 150/Arrays & Hashing/LongestConsecutiveSequence.py
+++ NeetCode 150/Arrays & Hashing/LongestConsecutiveSequence.py
@@ -1,17 +1,25 @@
 from typing import List
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        numset = set(nums)
-        result = 0
+        # nums = list(set(sorted(nums)))
+        # large = 1
+        # seq = 1
+        # for i in range(len(nums)-1):
+        #     if nums[i+1] == nums[i]+1:
+        #         seq+=1
+        #     else:
+        #         seq = 1
+        #     large = max(seq,large)
+        # return large
 
-        for i in numset:
-            if i-1 not in numset:
+        num_set = set(nums)
+        longest = 0
+
+        for num in num_set:
+            if (num - 1) not in num_set: # Identifying the start of sequence - num
                 length = 1
-                while i+length in numset:
-                    length +=1
-                result = max(length,result)
-        
-        return result
-
-
+                while (num + length) in num_set: # End of sequence is num + length
+                    length += 1
+                longest = max(length, longest)
+        return longest
         
