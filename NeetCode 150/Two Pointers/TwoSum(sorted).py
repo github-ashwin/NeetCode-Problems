c3fd7d2a6bf3a7bde62672e71e5aca4b08bbe22a
github-ashwin <ashi.github@gmail.com>
2025-07-27 20:00:32 +0530

    Two Integer Sum II

--- NeetCode 150/Two Pointers/TwoSum(sorted).py
+++ NeetCode 150/Two Pointers/TwoSum(sorted).py
@@ -1,16 +1,18 @@
 from typing import List
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        # Set pointers
         left = 0
         right = len(numbers)-1
 
-        while left < right:
+        while left < right: # Predefined condition
             if numbers[left] + numbers[right] > target:
-                right -=1
+                right-=1
             elif numbers[left] + numbers[right] < target:
-                left +=1
+                left+=1
             else:
                 return [left+1,right+1]
-        return []
+        return [] # Default
+        
 
         
