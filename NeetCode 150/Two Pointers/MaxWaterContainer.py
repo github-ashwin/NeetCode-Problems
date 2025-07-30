5dd85e125f68b673e8b984db5ef3c48eee4eedb1
github-ashwin <ashi.github@gmail.com>
2025-07-30 20:26:56 +0530

    Container with most water

--- NeetCode 150/Two Pointers/MaxWaterContainer.py
+++ NeetCode 150/Two Pointers/MaxWaterContainer.py
@@ -1,19 +1,31 @@
 from typing import List
 class Solution:
     def maxArea(self, heights: List[int]) -> int:
+        """
+        Bruteforce method
+
+        for left in range(len(heights)):
+            for right in range(l+1,len(heights)):
+                area = (right-left) * min(heights[left],heights[right])
+                result = max(area,result)
+        return result
+
+        """
         left = 0
         right = len(heights)-1
 
         result = 0
 
         while left < right:
-            val = min(heights[left],heights[right])*(right-left)
-            result = max(result,val)
+            # Shift pointers based on max. area
+            area = (right-left) * min(heights[left],heights[right])
+            result = max(area,result)
 
-            if heights[left] <= heights[right]:
-                left +=1
+            if heights[left] < heights[right]:
+                left+=1
             else:
-                right -=1
-        
+                right-=1
+
         return result
+        
         
