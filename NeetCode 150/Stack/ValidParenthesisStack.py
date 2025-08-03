a796f48aa0288137285791a226281e48cc976c8c
github-ashwin <ashi.github@gmail.com>
2025-08-03 09:53:48 +0530

    Valid Parenthesis

--- NeetCode 150/Stack/ValidParenthesisStack.py
+++ NeetCode 150/Stack/ValidParenthesisStack.py
@@ -1,11 +1,11 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        chars = {'(': ')', '[': ']', '{': '}'}
+        chars = {'(': ')', '[': ']', '{': '}'} # Valid Parenthesis pairs
         for c in s:
-            if c in chars:
+            if c in chars: # Checks if thr character is valid, then adds
                 stack.append(c)
             else:
-                if not stack or chars[stack.pop()]!=c:
+                if not stack or chars[stack.pop()]!=c: # If not valid or not the right sequence, return False
                     return False
         return not stack
