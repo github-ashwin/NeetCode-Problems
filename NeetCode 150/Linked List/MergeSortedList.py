ec58fd6552d70b2aaf66ff25cb644c5419b476e8
github-ashwin <ashi.github@gmail.com>
2025-08-08 06:08:21 +0530

    Merge Two Sorted Linked List

--- NeetCode 150/Linked List/MergeSortedList.py
+++ NeetCode 150/Linked List/MergeSortedList.py
@@ -1,31 +1,24 @@
-
-class ListNode(object):
+# Definition for singly-linked list.
+from typing import Optional
+class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-class Solution(object):
-    def mergeTwoLists(self, list1, list2):
-        """
-        :type list1: Optional[ListNode]
-        :type list2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
-        result = ListNode(-1)
-        current = result
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        head = new_node = ListNode() # new_node is moving pointer, head points to first position
 
         while list1 and list2:
-            if list1.val <= list2.val:
-                current.next = list1
+            if list1.val < list2.val:
+                new_node.next = list1
                 list1 = list1.next
             else:
-                current.next = list2
+                new_node.next = list2
                 list2 = list2.next
-            current = current.next
+            new_node = new_node.next
+        
+        new_node.next = list1 or list2 # merging remaining elements
 
-        if list1:
-            current.next = list1
-        else:
-            current.next = list2
-
-        return result.next
+        return head.next # return the real start after removing the dummy node
         
