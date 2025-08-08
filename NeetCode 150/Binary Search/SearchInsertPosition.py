d9146f883b8bfeb2b79a89fbb2a86f22535df5fd
github-ashwin <ashi.github@gmail.com>
2025-08-08 06:13:53 +0530

    Search Insert Position

---
+++ NeetCode 150/Binary Search/SearchInsertPosition.py
@@ -0,0 +1,16 @@
+from typing import List
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        result = len(nums) # by default last
+        left, right = 0, len(nums)-1
+
+        while left<=right:
+            mid = (left+right)//2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] > target:
+                result = mid
+                right = mid - 1
+            else:
+                left = mid + 1
+        return result
