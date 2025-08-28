f4869ce2cc0d599cff70da4d684c63d9bade77dc
Ashwin V Kumar <122103046+github-ashwin@users.noreply.github.com>
2025-08-28 19:45:33 +0530

    Kth largest element in an array - Max Heap approach

--- NeetCode 150/Kthlargestelementinanarray.py
+++ NeetCode 150/Kthlargestelementinanarray.py
@@ -1,6 +1,17 @@
-from typing import List
+# from typing import List
+# class Solution:
+    # def findKthLargest(self, nums: List[int], k: int) -> int:
+        # nums.sort()
+        # return nums[len(nums) - k]
+
+import heapq
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        nums.sort()
-        return nums[len(nums) - k]
+        maxheap = [-n for n in nums] # invert values
+        heapq.heapify(maxheap)
+
+        for i in range(k-1):
+            - heapq.heappop(maxheap)
+        
+        return -heapq.heappop(maxheap)
         
